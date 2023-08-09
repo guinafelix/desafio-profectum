871390f2751547e6c7516b311f94a41d10d951b1
guinafelix <oguilhermeleitefelix@gmail.com>
2023-08-09 19:05:49 -0300

    refactor: add imports in disciplina controller

--- src/main/java/com/profectum/desafio/controller/DisciplinaController.java
+++ src/main/java/com/profectum/desafio/controller/DisciplinaController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.profectum.desafio.dto.Disciplina.CriarDisciplinaDto;
 import com.profectum.desafio.models.Disciplina;
 import com.profectum.desafio.services.disciplinas.CriarDisciplina;
+import com.profectum.desafio.services.disciplinas.ListarDisciplinaPorId;
 import com.profectum.desafio.services.disciplinas.ListarDisciplinas;
 
 @RestController
